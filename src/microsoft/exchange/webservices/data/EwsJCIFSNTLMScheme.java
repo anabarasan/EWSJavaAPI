efd5c84672bfe980a347a6396b015fb25e80e2a9
Anbarasan <nasarabna@gmail.com>
2014-08-21 11:31:21 +0530

    fix for NTLM Auth Failure microsoft.exchange.webservices.data.EWSHttpException: Connection not established

--- src/microsoft/exchange/webservices/data/EwsJCIFSNTLMScheme.java
+++ src/microsoft/exchange/webservices/data/EwsJCIFSNTLMScheme.java
@@ -8,6 +8,8 @@ package microsoft.exchange.webservices.data;
 
 import java.io.IOException;
 
+import jcifs.ntlmssp.NtlmFlags;
+
 import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.NTCredentials;
@@ -199,40 +201,53 @@ public class EwsJCIFSNTLMScheme implements AuthScheme {
 		}
 	}
 
+	/*
+	 * Modifications based on Stack overflow question for NTLM AUTH FAILURE
+	 * http://stackoverflow.com/questions/14702764/how-to-use-ldap-authentication-for-the-exchange-web-services-connection-in-java
+	 */
 	private class NTLM {
-		/** Character encoding */
-		public static final String DEFAULT_CHARSET = "ASCII";
+	    /** Character encoding */
+	    public static final String DEFAULT_CHARSET = "ASCII";
 
-		/**
-		 * The character was used by 3.x's NTLM to encode the username and
-		 * password. Apparently, this is not needed in when passing username,
-		 * password from NTCredentials to the JCIFS library
-		 */
-		private String credentialCharset = DEFAULT_CHARSET;
+	    /**
+	    * The character was used by 3.x's NTLM to encode the username and
+	    * password. Apparently, this is not needed in when passing username,
+	    * password from NTCredentials to the JCIFS library
+	    */
+	    private String credentialCharset = DEFAULT_CHARSET;
 
-		void setCredentialCharset(String credentialCharset) {
-			this.credentialCharset = credentialCharset;
-		}
+	    void setCredentialCharset(String credentialCharset) {
+	           this.credentialCharset = credentialCharset;
+	    }
 
-		private String generateType1Msg(String host, String domain) {
-			jcifs.ntlmssp.Type1Message t1m = new jcifs.ntlmssp.Type1Message(jcifs.ntlmssp.Type1Message.getDefaultFlags(),
-					domain, host);
-			return jcifs.util.Base64.encode(t1m.toByteArray());
-		}
+	    private static final int TYPE_1_FLAGS = NtlmFlags.NTLMSSP_NEGOTIATE_NTLM
+	                 | NtlmFlags.NTLMSSP_NEGOTIATE_UNICODE
+	                 | NtlmFlags.NTLMSSP_NEGOTIATE_NTLM2;
 
-		private String generateType3Msg(String username, String password, String host,
-				String domain, String challenge) {
-			jcifs.ntlmssp.Type2Message t2m;
-			try {
-				t2m = new jcifs.ntlmssp.Type2Message(jcifs.util.Base64.decode(challenge));
-			} catch (IOException e) {
-				throw new RuntimeException("Invalid Type2 message", e);
-			}
+	    private String generateType1Msg(String host, String domain) {
+	           jcifs.ntlmssp.Type1Message t1m = new jcifs.ntlmssp.Type1Message(
+	                        TYPE_1_FLAGS, domain, host);
+	           return jcifs.util.Base64.encode(t1m.toByteArray());
+	    }
 
-			jcifs.ntlmssp.Type3Message t3m = new jcifs.ntlmssp.Type3Message(t2m, password, domain,
-					username, host, 0);
-			return jcifs.util.Base64.encode(t3m.toByteArray());
-		}
+	    private String generateType3Msg(String username, String password,
+	                 String host, String domain, String challenge) {
+	           jcifs.ntlmssp.Type2Message t2m;
+	           try {
+	                 t2m = new jcifs.ntlmssp.Type2Message(
+	                               jcifs.util.Base64.decode(challenge));
+	           } catch (IOException e) {
+	                 throw new RuntimeException("Invalid Type2 message", e);
+	           }
+
+	           final int type2Flags = t2m.getFlags();
+	           final int type3Flags = type2Flags
+	                        & (0xffffffff ^ (NtlmFlags.NTLMSSP_TARGET_TYPE_DOMAIN | NtlmFlags.NTLMSSP_TARGET_TYPE_SERVER));
+
+	           jcifs.ntlmssp.Type3Message t3m = new jcifs.ntlmssp.Type3Message(
+	                        t2m, password, domain, username, host, type3Flags);
+	           return jcifs.util.Base64.encode(t3m.toByteArray());
+	    }
 	}
 }
 
